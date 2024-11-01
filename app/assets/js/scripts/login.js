efcc6946240b9f5879b54029da4ae117448ffd25
Envel <n.gucenkov@gmail.com>
2024-11-01 22:50:09 +0300

    demo v2

--- app/assets/js/scripts/login.js
+++ app/assets/js/scripts/login.js
@@ -2,25 +2,21 @@
  * Script for login.ejs
  */
 // Validation Regexes.
-const validUsername         = /^[a-zA-Z0-9_]{1,16}$/
-const basicEmail            = /^\S+@\S+\.\S+$/
-//const validEmail          = /^(([^<>()\[\]\.,;:\s@\"]+(\.[^<>()\[\]\.,;:\s@\"]+)*)|(\".+\"))@(([^<>()[\]\.,;:\s@\"]+\.)+[^<>()[\]\.,;:\s@\"]{2,})$/i
+const validUsername = /^[a-zA-Z0-9_]{4,16}$/;
+const basicEmail = /^\S+@\S+\.\S+$/;
 
 // Login Elements
-const loginCancelContainer  = document.getElementById('loginCancelContainer')
-const loginCancelButton     = document.getElementById('loginCancelButton')
-const loginEmailError       = document.getElementById('loginEmailError')
-const loginUsername         = document.getElementById('loginUsername')
-const loginPasswordError    = document.getElementById('loginPasswordError')
-const loginPassword         = document.getElementById('loginPassword')
-const checkmarkContainer    = document.getElementById('checkmarkContainer')
-const loginRememberOption   = document.getElementById('loginRememberOption')
-const loginButton           = document.getElementById('loginButton')
-const loginForm             = document.getElementById('loginForm')
+const loginCancelContainer  = document.getElementById('loginCancelContainer');
+const loginCancelButton     = document.getElementById('loginCancelButton');
+const loginEmailError       = document.getElementById('loginEmailError');
+const loginUsername         = document.getElementById('loginUsername');
+const checkmarkContainer    = document.getElementById('checkmarkContainer');
+const loginRememberOption   = document.getElementById('loginRememberOption');
+const loginButton           = document.getElementById('loginButton');
+const loginForm             = document.getElementById('loginForm');
 
 // Control variables.
-let lu = false, lp = false
-
+let lu = false;
 
 /**
  * Show a login error.
@@ -29,8 +25,8 @@ let lu = false, lp = false
  * @param {string} value The error text.
  */
 function showError(element, value){
-    element.innerHTML = value
-    element.style.opacity = 1
+    element.innerHTML = value;
+    element.style.opacity = 1;
 }
 
 /**
@@ -40,73 +36,45 @@ function showError(element, value){
  */
 function shakeError(element){
     if(element.style.opacity == 1){
-        element.classList.remove('shake')
-        void element.offsetWidth
-        element.classList.add('shake')
+        element.classList.remove('shake');
+        void element.offsetWidth;
+        element.classList.add('shake');
     }
 }
 
 /**
- * Validate that an email field is neither empty nor invalid.
+ * Validate input for username or email.
  * 
- * @param {string} value The email value.
+ * @param {string} value The value to validate.
  */
-function validateEmail(value){
-    if(value){
-        if(!basicEmail.test(value) && !validUsername.test(value)){
-            showError(loginEmailError, Lang.queryJS('login.error.invalidValue'))
-            loginDisabled(true)
-            lu = false
+function validateInput(value) {
+    if (value) {
+        if (!basicEmail.test(value) && !validUsername.test(value)) {
+            showError(loginEmailError, Lang.queryJS('login.error.invalidValue'));
+            loginDisabled(true);
+            lu = false;
         } else {
-            loginEmailError.style.opacity = 0
-            lu = true
-            if(lp){
-                loginDisabled(false)
-            }
+            loginEmailError.style.opacity = 0;
+            lu = true;
+            loginDisabled(false);
         }
     } else {
-        lu = false
-        showError(loginEmailError, Lang.queryJS('login.error.requiredValue'))
-        loginDisabled(true)
-    }
-}
-
-/**
- * Validate that the password field is not empty.
- * 
- * @param {string} value The password value.
- */
-function validatePassword(value){
-    if(value){
-        loginPasswordError.style.opacity = 0
-        lp = true
-        if(lu){
-            loginDisabled(false)
-        }
-    } else {
-        lp = false
-        showError(loginPasswordError, Lang.queryJS('login.error.invalidValue'))
-        loginDisabled(true)
+        lu = false;
+        showError(loginEmailError, Lang.queryJS('login.error.requiredValue'));
+        loginDisabled(true);
     }
 }
 
 // Emphasize errors with shake when focus is lost.
 loginUsername.addEventListener('focusout', (e) => {
-    validateEmail(e.target.value)
-    shakeError(loginEmailError)
-})
-loginPassword.addEventListener('focusout', (e) => {
-    validatePassword(e.target.value)
-    shakeError(loginPasswordError)
-})
+    validateInput(e.target.value);
+    shakeError(loginEmailError);
+});
 
-// Validate input for each field.
+// Validate input on each keystroke.
 loginUsername.addEventListener('input', (e) => {
-    validateEmail(e.target.value)
-})
-loginPassword.addEventListener('input', (e) => {
-    validatePassword(e.target.value)
-})
+    validateInput(e.target.value);
+});
 
 /**
  * Enable or disable the login button.
@@ -115,7 +83,7 @@ loginPassword.addEventListener('input', (e) => {
  */
 function loginDisabled(v){
     if(loginButton.disabled !== v){
-        loginButton.disabled = v
+        loginButton.disabled = v;
     }
 }
 
@@ -126,11 +94,11 @@ function loginDisabled(v){
  */
 function loginLoading(v){
     if(v){
-        loginButton.setAttribute('loading', v)
-        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.login'), Lang.queryJS('login.loggingIn'))
+        loginButton.setAttribute('loading', v);
+        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.login'), Lang.queryJS('login.loggingIn'));
     } else {
-        loginButton.removeAttribute('loading')
-        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.loggingIn'), Lang.queryJS('login.login'))
+        loginButton.removeAttribute('loading');
+        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.loggingIn'), Lang.queryJS('login.login'));
     }
 }
 
@@ -140,95 +108,85 @@ function loginLoading(v){
  * @param {boolean} v True to enable, false to disable.
  */
 function formDisabled(v){
-    loginDisabled(v)
-    loginCancelButton.disabled = v
-    loginUsername.disabled = v
-    loginPassword.disabled = v
+    loginDisabled(v);
+    loginCancelButton.disabled = v;
+    loginUsername.disabled = v;
     if(v){
-        checkmarkContainer.setAttribute('disabled', v)
+        checkmarkContainer.setAttribute('disabled', v);
     } else {
-        checkmarkContainer.removeAttribute('disabled')
+        checkmarkContainer.removeAttribute('disabled');
     }
-    loginRememberOption.disabled = v
+    loginRememberOption.disabled = v;
 }
 
-let loginViewOnSuccess = VIEWS.landing
-let loginViewOnCancel = VIEWS.settings
-let loginViewCancelHandler
+let loginViewOnSuccess = VIEWS.landing;
+let loginViewOnCancel = VIEWS.settings;
+let loginViewCancelHandler;
 
 function loginCancelEnabled(val){
     if(val){
-        $(loginCancelContainer).show()
+        $(loginCancelContainer).show();
     } else {
-        $(loginCancelContainer).hide()
+        $(loginCancelContainer).hide();
     }
 }
 
 loginCancelButton.onclick = (e) => {
     switchView(getCurrentView(), loginViewOnCancel, 500, 500, () => {
-        loginUsername.value = ''
-        loginPassword.value = ''
-        loginCancelEnabled(false)
+        loginUsername.value = '';
+        loginCancelEnabled(false);
         if(loginViewCancelHandler != null){
-            loginViewCancelHandler()
-            loginViewCancelHandler = null
+            loginViewCancelHandler();
+            loginViewCancelHandler = null;
         }
-    })
+    });
 }
 
 // Disable default form behavior.
-loginForm.onsubmit = () => { return false }
+loginForm.onsubmit = () => { return false; };
 
 // Bind login button behavior.
 loginButton.addEventListener('click', () => {
     // Disable form.
-    formDisabled(true)
+    formDisabled(true);
 
-    // Show loading stuff.
-    loginLoading(true)
+    // Show loading animation.
+    loginLoading(true);
 
-    AuthManager.addMojangAccount(loginUsername.value, loginPassword.value).then((value) => {
-        updateSelectedAccount(value)
-        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.loggingIn'), Lang.queryJS('login.success'))
-        $('.circle-loader').toggleClass('load-complete')
-        $('.checkmark').toggle()
+    // Attempt login with empty password.
+    AuthManager.addMojangAccount(loginUsername.value, "").then((value) => {
+        updateSelectedAccount(value);
+        loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.loggingIn'), Lang.queryJS('login.success'));
+        $('.circle-loader').toggleClass('load-complete');
+        $('.checkmark').toggle();
         setTimeout(() => {
             switchView(VIEWS.login, loginViewOnSuccess, 500, 500, async () => {
-                // Temporary workaround
                 if(loginViewOnSuccess === VIEWS.settings){
-                    await prepareSettings()
+                    await prepareSettings();
                 }
-                loginViewOnSuccess = VIEWS.landing // Reset this for good measure.
-                loginCancelEnabled(false) // Reset this for good measure.
-                loginViewCancelHandler = null // Reset this for good measure.
-                loginUsername.value = ''
-                loginPassword.value = ''
-                $('.circle-loader').toggleClass('load-complete')
-                $('.checkmark').toggle()
-                loginLoading(false)
-                loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.success'), Lang.queryJS('login.login'))
-                formDisabled(false)
-            })
-        }, 1000)
+                loginViewOnSuccess = VIEWS.landing;
+                loginCancelEnabled(false);
+                loginViewCancelHandler = null;
+                loginUsername.value = '';
+                $('.circle-loader').toggleClass('load-complete');
+                $('.checkmark').toggle();
+                loginLoading(false);
+                loginButton.innerHTML = loginButton.innerHTML.replace(Lang.queryJS('login.success'), Lang.queryJS('login.login'));
+                formDisabled(false);
+            });
+        }, 1000);
     }).catch((displayableError) => {
-        loginLoading(false)
+        loginLoading(false);
 
-        let actualDisplayableError
-        if(isDisplayableError(displayableError)) {
-            msftLoginLogger.error('Error while logging in.', displayableError)
-            actualDisplayableError = displayableError
-        } else {
-            // Uh oh.
-            msftLoginLogger.error('Unhandled error during login.', displayableError)
-            actualDisplayableError = Lang.queryJS('login.error.unknown')
-        }
+        let actualDisplayableError = isDisplayableError(displayableError)
+            ? displayableError
+            : Lang.queryJS('login.error.unknown');
 
-        setOverlayContent(actualDisplayableError.title, actualDisplayableError.desc, Lang.queryJS('login.tryAgain'))
+        setOverlayContent(actualDisplayableError.title, actualDisplayableError.desc, Lang.queryJS('login.tryAgain'));
         setOverlayHandler(() => {
-            formDisabled(false)
-            toggleOverlay(false)
-        })
-        toggleOverlay(true)
-    })
-
-})
+            formDisabled(false);
+            toggleOverlay(false);
+        });
+        toggleOverlay(true);
+    });
+});
