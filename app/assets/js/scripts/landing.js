3b7de64126c834f0fa3365616f8c6393a6211e0c
Envel <n.gucenkov@gmail.com>
2025-01-23 16:21:02 +0300

    1

--- app/assets/js/scripts/landing.js
+++ app/assets/js/scripts/landing.js
@@ -364,12 +364,18 @@ async function asyncSystemScan(effectiveJavaOptions, launchAfter = true){
 
 async function downloadJava(effectiveJavaOptions, launchAfter = true) {
 
+
+    const minecraftVersion = this.server.rawServer.minecraftVersion;
+    const isVersionBelow120 = mcVersionAtLeast('1.20', minecraftVersion) === false;
+
     // TODO Error handling.
     // asset can be null.
     const asset = await latestOpenJDK(
         effectiveJavaOptions.suggestedMajor,
         ConfigManager.getDataDirectory(),
-        effectiveJavaOptions.distribution)
+        effectiveJavaOptions.distribution,
+        isVersionBelow120
+    );
 
     if(asset == null) {
         throw new Error(Lang.queryJS('landing.downloadJava.findJdkFailure'))
