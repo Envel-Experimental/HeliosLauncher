b2ce51d4ca90c54a29404a1efb40206e380e9ad8
Envel <n.gucenkov@gmail.com>
2026-06-22 16:38:56 +0300

    rolling update fixes

--- app/assets/js/ui/views/settings.js
+++ app/assets/js/ui/views/settings.js
@@ -2481,7 +2481,7 @@ export function populateSettingsUpdateInformation(data) {
         settingsUpdateButtonStatus(Lang.queryJS('settings.updates.checkForUpdatesButton'), false, () => {
             const _isDev = window.isDev || false
             if (!_isDev) {
-                ipcRenderer.send('autoUpdateAction', 'checkForUpdate')
+                ipcRenderer.send('autoUpdateAction', 'checkForUpdate', ConfigManager.getAllowPrerelease())
                 settingsUpdateButtonStatus(Lang.queryJS('settings.updates.checkingForUpdatesButton'), true)
                 
                 // Safety timeout: reset button if no response within 15 seconds
